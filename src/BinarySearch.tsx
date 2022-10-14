26ec0fd8992619411ad7d88f61ec459e458ef9c0
IbrahimAlshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-10-14 09:38:58 +0300

    Implmented binarySearch

---
+++ src/BinarySearch.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export function BinarySearch() : JSX.Element {
+
+    const n : number = 1024;
+    const [stepCount, setStepCount] = useState(0);
+    const [hi, setHi] = useState(n);
+    const [lo, setLo] = useState(0);
+    const [needleIdx, setNeedleIdx] = useState(Math.floor(Math.random() * n));
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            step();
+            setStepCount(stepCount+1);
+        }, 1000);
+        return () => clearInterval(interval);
+      }, [lo, hi]);
+
+    
+    function step() {
+        const currIdx = lo + Math.floor((hi - lo) / 2);
+        if (hayStack[currIdx] === hayStack[needleIdx]) {
+            console.log("bingo", stepCount)
+            return;
+        }
+        if (hayStack[currIdx] < hayStack[needleIdx]) {
+            setLo(currIdx + 1);
+        } else {
+            setHi(currIdx);
+        }
+    }
+    const hayStack : number[] = [];
+    for (let i = 0; i < n; i++) {
+        hayStack.push(i);
+    }
+
+    const entries : JSX.Element[] = [];
+    const currIdx = lo + Math.floor((hi - lo) / 2);
+    for (let i = 0; i < hayStack.length; i++) {
+        
+        let color = "gray";
+        if (hayStack[needleIdx] === hayStack[i]) {
+            color = 'green';
+        }
+        if (i === currIdx) {
+            color = 'red';
+        }
+
+        const entryStyle = {
+            height : hayStack[i],
+            backgroundColor : color
+        }
+        entries.push(<div className="entry" style={entryStyle}/>);
+    }
+    return <div className="entries">{entries}</div>;
+}
